cb2fcb4111e674e8631f92ab6f16df76f8160a48
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-05-12 11:11:06 +0200

    feat(cmd) generate helper

--- cmd.go
+++ cmd.go
@@ -1,5 +1,7 @@
 package cliapp_maker
 
+import "fmt"
+
 type Cmd struct {
 	global
 	Options []Option
@@ -11,4 +13,20 @@ type CmdData struct {
 	Name          string
 	OptionsPassed []OptionPassed
 	ParamsPassed  []ParamPassed
+}
+
+func (cmd Cmd) generateHelp() {
+	fmt.Printf("%s - %s\n", cmd.Name, cmd.Help)
+	params := ""
+	for _, v := range cmd.Params {
+		params += " " + v.Name + " (" + v.ParamType.String() + ")"
+	}
+	fmt.Printf("%s - %s\n", cmd.Name, params)
+	for _, opt := range cmd.Options {
+		if opt.TakeValue {
+			fmt.Printf("--%s %s - %s\n", opt.Name, opt.OptType.String(), opt.Help)
+			continue
+		}
+		fmt.Printf("-%s - %s\n", opt.Name, opt.Help)
+	}
 }
