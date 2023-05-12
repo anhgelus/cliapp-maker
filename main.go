55f69a30c96c0b51e9570c61ff4058a299d2b06d
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-05-12 11:57:53 +0200

    feat(main) parse options

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package cliapp_maker
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
 
 type global struct {
 	Name string
@@ -12,6 +17,24 @@ type CliApp struct {
 	Version     string
 	VersionNote string
 	Cmds        []Cmd
+}
+
+func (app CliApp) Handle() {
+	cli := ""
+	for i := 1; i < len(os.Args)-2; i++ {
+		cli += " " + os.Args[0]
+	}
+	option := regexp.MustCompile(`--[a-zA-Z\-]+ [a-zA-Z0-9 ]+`)
+	simpleOption := regexp.MustCompile(`-[a-zA-Z]+`)
+	opts := option.FindAllString(cli, -1)
+	nCli := cli
+	for _, o := range opts {
+		nCli = strings.ReplaceAll(cli, o, "")
+	}
+	simpleOpts := option.FindAllString(nCli, -1)
+	//TODO: get every options' struct
+	//TODO: put them into options
+	//TODO: parse the command and parameters
 }
 
 func (app CliApp) generateHelp() {
